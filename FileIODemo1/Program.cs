39d5c0f87806eaba6d5db2ed7f522530e6fea008
PSarvesh <PSarvesh@OPT-LAP-0651>
2022-08-11 10:45:56 +0530

    FileIO1

---
+++ FileIODemo1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//collect person name and store in file
+using System.IO;
+
+
+StreamReader reader = new StreamReader("D://nameslist.txt");
+//string name = reader.ReadLine();    
+string allName = reader.ReadToEnd();
+reader.Close();
+Console.WriteLine(allName);
+
+
+static void SaveData()
+{
+    StreamWriter write = null;
+    try
+    {
+
+        Console.WriteLine("ENter Person Name");
+
+        string pName = Console.ReadLine();
+
+        StreamWriter writer = new StreamWriter("D://nameslist.txt", true);
+        writer.WriteLine(pName);
+    }
+    //catch
+    //{
+    //    write.Close();
+    //}
+    finally
+    {
+        write.Close();
+    }
+}
